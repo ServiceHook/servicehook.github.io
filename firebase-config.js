dcfa7f9ffed619c335a5ee90d805c184d1ee8a80
servicehook <kamarjahankammu@gmail.com>
2025-05-22 23:57:05 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyD0OdRSvkRRZN_UOR91Pc8sqyRwc_jC6ZU",
   authDomain: "servicehook.firebaseapp.com",
+  databaseURL: "https://servicehook.firebaseio.com",
   projectId: "servicehook",
   storageBucket: "servicehook.firebasestorage.app",
   messagingSenderId: "352210796691",
@@ -16,6 +12,5 @@ const firebaseConfig = {
   measurementId: "G-PE6M06CB7X"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
